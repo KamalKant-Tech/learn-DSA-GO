3efd3e418a419e6cac681529c6552cb73be8d2c3
Kamal <kamalkhn888@gmail.com>
2025-03-11 10:40:27 +0530

    add few more dp solution

--- DP/Medium/medium_dp.go
+++ DP/Medium/medium_dp.go
@@ -31,6 +31,7 @@ func main() {
 	// fmt.Println(numSquaresTabulation(18))
 	fmt.Println(totalUniquePathsMemo(2, 6))
 	fmt.Println(totalUniquePathsTabulation(2, 6))
+	fmt.Println(totalUniquePathsSpaceOptimization(2, 6))
 }
 
 // Problem: 2498. Frog Jump II
@@ -660,21 +661,58 @@ func totalUniquePathsMemoHelper(row, col int, dp [][]int) int {
 }
 
 // Using Tabulation
-// TC: O(m*n), SC: O((m-1) + (n-1)) + O(n * m)
+// TC: O(m*n), SC: O(n * m)
 func totalUniquePathsTabulation(m, n int) int {
 	dp := make([][]int, m)
 	for i := 0; i < m; i++ {
 		dp[i] = make([]int, n)
 	}
 	dp[0][0] = 1
-	fmt.Println("Dp Array: ", dp)
 	for row := 0; row < m; row++ {
 		for col := 0; col < n; col++ {
-			if row-1 < 0 || col-1 < 0 {
+			up := 0
+			left := 0
+			if row == 0 && col == 0 {
 				continue
+			} else {
+				if row > 0 {
+					up = dp[row-1][col]
+				}
+
+				if col > 0 {
+					left = dp[row][col-1]
+				}
+				dp[row][col] = up + left
 			}
-			dp[row][col] = dp[row-1][col] + dp[row][col-1]
 		}
 	}
 	return dp[m-1][n-1]
+}
+
+// Using Space Optimization
+// TC: O(m*n), SC: O(n)
+func totalUniquePathsSpaceOptimization(m, n int) int {
+	prevRow := make([]int, n) // take one dummy row for to refer the the up values
+	for row := 0; row < m; row++ {
+		currentRow := make([]int, n) // Take one more dummy array to keep track of current row calculation and store it.
+		for col := 0; col < n; col++ {
+			up := 0
+			left := 0
+			if row == 0 && col == 0 {
+				currentRow[0] = 1
+				continue
+			} else {
+				if row > 0 {
+					up = prevRow[col]
+				}
+
+				if col > 0 {
+					left = currentRow[col-1]
+				}
+				currentRow[col] = up + left
+			}
+		}
+		prevRow = currentRow
+	}
+	return prevRow[n-1]
 }
